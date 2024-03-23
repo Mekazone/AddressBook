a60afd285ebb4c86abaa233c9df528b7976f37aa
Emeka Ozonze <mekaz.ozonze@gmail.com>
2024-03-23 02:30:48 +0000

    Initial Commit

---
+++ Applify/Applify/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Applify.Models;
+using SQLite;
+using System.IO;
+
+namespace Applify
+{
+    public partial class MainPage : ContentPage
+    {
+        private SQLiteAsyncConnection conn;
+        public MainPage()
+        {
+            InitializeComponent();
+            var documentPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var databasePath = Path.Combine(documentPath, "Contacts.db");
+            conn = new SQLiteAsyncConnection(databasePath);
+
+            conn.CreateTableAsync<Contacts>();
+            ReadData();
+        }
+        private ObservableCollection<Contacts> cList;
+        public ObservableCollection<Contacts> CList
+        {
+            get { return cList; }
+            set { cList = value; }
+        }
+        private void Save(object sender, EventArgs e)
+        {
+            conn.InsertAsync(new Contacts() { Name = Name.Text, Address = Address.Text });
+
+            ReadData();
+        }
+        public void ReadData()
+        {
+
+            var list = conn.Table<Contacts>().ToListAsync().Result;
+
+            CList = new ObservableCollection<Contacts>(list);
+            ContactList.ItemsSource = CList;
+        }
+        private void ListItem_Delete(object sender, EventArgs e)
+        {
+            var mi = sender as MenuItem;
+            var item = mi.CommandParameter as Contacts;
+            conn.DeleteAsync(item);
+            ReadData();
+        }
+        private void Update(object sender, EventArgs e)
+        {
+            var mi = sender as MenuItem;
+            var item = mi.CommandParameter as Contacts;
+            item.Name = "Martin";
+            item.Address = "New York";
+            conn.UpdateAsync(item);
+            ReadData();
+        }
+    }
+}
---
+++ Applify/Applify/Models/Contacts.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Applify.Models
+{
+    public class Contacts
+    {
+        private int id;
+        [PrimaryKey, AutoIncrement]
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private string address;
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+    }
+}
